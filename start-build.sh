7a94b32193e971c1533f5de1a2bbbddb79d9e56f
Miheer Salunke <miheer.salunke@gmail.com>
2019-10-04 19:37:15 +1000

    Temporary

---
+++ start-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+oc -n openshift-ingress-operator start-build ingress-operator \
+   ${V+--follow} --wait
+
+if [[ -n "${DEPLOY+1}" ]]
+then
+    oc -n openshift-ingress-operator patch deploy/ingress-operator \
+       --type=strategic --patch='
+{
+  "spec": {
+    "template": {
+      "spec": {
+        "containers": [
+          {
+            "name": "ingress-operator",
+            "image": "image-registry.openshift-image-registry.svc:5000/openshift-ingress-operator/ingress-operator:latest",
+            "imagePullPolicy": "Always"
+          }
+        ]
+      }
+    }
+  }
+}
+'
+fi
